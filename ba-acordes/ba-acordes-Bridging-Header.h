32a30ef1dfa122f6f3f45dbcf57caa046d51c12b
Davo <davidgalavotti@gmail.com>
2015-11-28 17:36:20 -0300

    Agrego bridgefile

---
+++ ba-acordes/ba-acordes-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  ba-acordes-Bridging-Header.h
+//  ba-acordes
+//
+//  Created by Davo on 11/28/15.
+//  Copyright © 2015 Pixelbeat. All rights reserved.
+//
+
+#ifndef ba_acordes_Bridging_Header_h
+#define ba_acordes_Bridging_Header_h
+
+#import "AKFoundation.h"
+
+#endif /* ba_acordes_Bridging_Header_h */
