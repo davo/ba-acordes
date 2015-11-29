91d44db108a9e5169509ae5d07040de2c8859fba
Mariano Molina <xsin.crueldadx@gmail.com>
2015-11-29 00:07:02 -0300

    Se fixeo el team para compilar en ipad, nuevo LaunchScreen

--- ba-acordes/ViewController.swift
+++ ba-acordes/ViewController.swift
@@ -7,19 +7,23 @@
 //
 
 import UIKit
+import AudioKit
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func tap(sender: AnyObject) {
+        
+    }
+ 
 }
+
 
